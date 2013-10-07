efc662f9241fc474d3557c72fe78c8d70ec2648b
Parfenar <parfenar@server.com>
2013-10-06 21:21:52 -0700

    I love Hailee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp2::Application.config.secret_token = '75132946d444383177108285e0b362167deff7430ee39c43da1f97626fe8af099cab300dd0f6bb353eb9df90355130760e5e038350a8aa3bdeea6019834c0b47'
